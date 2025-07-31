c6423b81794ed7a4398c17c29e6244c55b97347c
Amsal Adrian Ginting <143935429+amsaladrian@users.noreply.github.com>
2025-07-31 18:32:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,28 @@ window.onload = () => {
   const playBtn = document.getElementById('play-music');
   const titleElement = document.getElementById('title');
   music.loop = true;
+
+  // Tampilkan teks satu per satu (fungsi disiapkan dulu)
+  const titles = ('Happy girlfriend day sayangkuu, love u so much angelytas cantikkuu, hope u like it :) <3').split('');
+  let index = 0;
+
+  function appendTitle() {
+    if (index < titles.length) {
+      titleElement.innerHTML += titles[index];
+      index++;
+      setTimeout(appendTitle, 150);
+    }
+  }
+
   // Ketika tombol diklik:
   playBtn.addEventListener('click', () => {
     // Play musik
     music.muted = false;
-    music.play().catch(e => {
-      console.log("Autoplay dicegah:", e);
-    });
+    music.play();
 
     // Mulai animasi & ketik teks
     document.body.classList.remove("not-loaded");
     playBtn.style.display = 'none'; // Sembunyikan tombol
-
-    // Tampilkan teks satu per satu
-    const titles = ('Happy girlfriend day sayangkuu, love u so much angelytas cantikkuu, hope u like it :) <3').split('');
-    let index = 0;
-
-    function appendTitle() {
-      if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
-        index++;
-        setTimeout(appendTitle, 150);
-      }
-    }
 
     appendTitle();
   });
